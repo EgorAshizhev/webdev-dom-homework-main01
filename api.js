9e27aeda329fa7d6ad3b399bf0ce1309ae558ed4
EgorAshizhev <egor.ashizhev.1997@mail.ru>
2026-03-21 22:02:56 +0300

    add podt500

--- api.js
+++ api.js
@@ -1,43 +1,52 @@
 import { getCurrentDate } from "./date.js";
 
-const host = "https://wedev-api.sky.pro/api/v1/EgorAshizhev"
+const host = "https://wedev-api.sky.pro/api/v1/EgorAshizhev";
 
 export const fetchComments = () => {
     return fetch(host + "/comments")
-    .then((res) => {
-        if (!res.ok) {
-            throw new Error("Ошибка загрузки комментариев");
-        }
-        return res.json()
-    })
-    .then((responseData) => {
-       const appComments = responseData.comments.map(comment => {
-            return {
-                id: comment.id,
-                name: comment.author.name,
-                date: getCurrentDate(new Date(comment.date)),
-                text: comment.text,
-                likes: comment.likes,
-                isLiked: false,
+        .then((res) => {
+            if (!res.ok) {
+                throw new Error("Ошибка загрузки комментариев");
             }
+            return res.json();
         })
-        return appComments      
-    })
-}
+        .then((responseData) => {
+            const appComments = responseData.comments.map(comment => {
+                return {
+                    id: comment.id,
+                    name: comment.author.name,
+                    date: getCurrentDate(new Date(comment.date)),
+                    text: comment.text,
+                    likes: comment.likes,
+                    isLiked: false,
+                };
+            });
+            return appComments;
+        });
+};
 
 export const postComment = (text, name) => {
-  return fetch(host + '/comments', {
-    method: 'POST',
-    body: JSON.stringify({
-      text,
-      name,
-    }),
-  }).then((response) => {
-    if (!response.ok) {
-      return response.json().then(err => {
-        throw new Error(err.error || "Ошибка добавления комментария");
-      });
-    }
-    return fetchComments();
-  })
-}
+    return fetch(host + '/comments', {
+        method: 'POST',
+        body: JSON.stringify({
+            text,
+            name,
+            forceError: true,
+        }),
+    }).then((response) => {
+        if (!response.ok) {
+      
+            return response.text().then(body => {
+                let message;
+                try {
+                    const err = JSON.parse(body);
+                    message = err.error || "Ошибка добавления комментария";
+                } catch {
+                    message = body || "Ошибка добавления комментария";
+                }
+                throw new Error(message);
+            });
+        }
+        return fetchComments();
+    });
+};
